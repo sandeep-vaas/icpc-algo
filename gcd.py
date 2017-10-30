2b49b6bbeac61383ed2c350b9d59b980a2b139cd
DulajMadushanka <dulajgunawardana78@gmail.com>
2017-10-31 01:57:28 +0530

    Add gcd python

---
+++ gcd.py
@@ -0,0 +1,9 @@
+def gcd(a, b):
+    """Calculate the Greatest Common Divisor of a and b.
+
+    Unless b==0, the result will have the same sign as b (so that when
+    b is divided by it, the result comes out positive).
+    """
+    while b:
+        a, b = b, a%b
+    return a
